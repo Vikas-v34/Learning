6f97471876633f1df3ecfac1918c1582f19d0034
kacharerohan <61958001+kacharerohan@users.noreply.github.com>
2020-03-29 21:16:14 +0530

    Add files via upload

---
+++ Practice/src/com/practice/thouseef/Employee.java
@@ -0,0 +1,44 @@
+package com.capgemini.model;
+
+public class Employee  {
+
+	private int employeeid;
+	private String employeeName;
+	private double employeeSalary;
+	public int getEmployeeid() {
+		return employeeid;
+	}
+	public void setEmployeeid(int employeeid) {
+		this.employeeid = employeeid;
+	}
+	public String getEmployeeName() {
+		return employeeName;
+	}
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+	public double getEmployeeSalary() {
+		return employeeSalary;
+	}
+	public void setEmployeeSalary(double employeeSalary) {
+		this.employeeSalary = employeeSalary;
+	}
+	@Override
+	public String toString() {
+		return "Employee [employeeid=" + employeeid + ", employeeName="
+				+ employeeName + ", employeeSalary=" + employeeSalary + "]";
+	}
+	public Employee(int employeeid, String employeeName, double employeeSalary) {
+		super();
+		this.employeeid = employeeid;
+		this.employeeName = employeeName;
+		this.employeeSalary = employeeSalary;
+	}
+	public Employee(){
+		
+	}
+	
+	
+	
+	
+}
